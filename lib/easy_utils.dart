53effc9e74eb52ff38fa00824269adff8a1c1afe
alexpavelm <alexpavelm@gmail.com>
2022-03-31 20:34:04 +0300

    add exports

--- lib/easy_utils.dart
+++ lib/easy_utils.dart
@@ -1,7 +1,11 @@
 library easy_utils;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'src/logic/theme/app_themes.dart';
+export 'src/logic/bloc_provider_page.dart';
+export 'src/logic/multi_bloc_provider_page.dart';
+export 'src/ui/avatar_container.dart';
+export 'src/ui/close_button.dart';
+export 'src/ui/h_space.dart';
+export 'src/ui/w_space.dart';
+export 'src/ui/rounded_button.dart';
+export 'src/ui/rounded_container.dart';
