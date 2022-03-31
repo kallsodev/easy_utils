1ef5037de306fafe1f1452c07dc5dda5f65d93c3
alexpavelm <alexpavelm@gmail.com>
2022-03-31 21:20:34 +0300

    add easytheme init

--- lib/src/logic/easy_theme.dart
+++ lib/src/logic/easy_theme.dart
@@ -4,7 +4,17 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class EasyTheme {
+  EasyTheme._();
+
   static switchTheme(BuildContext context, bool isDarkMode) {
     context.read<ThemeCubit>().setTheme(isDarkMode);
+  }
+
+  static init({required AppThemes appThemes, required Widget child}) {
+    return BlocProviderPage(
+      scaffold: false,
+      bloc: ThemeCubit(appThemes),
+      child: child,
+    );
   }
 }
